2db62b964517421cfdfb4bfba5b9b4f6dab2e312
Hariharan A <110531320+hariharan102@users.noreply.github.com>
2023-09-19 19:47:35 +0530

    Add files via upload

---
+++ Day4/demo1/src/App.js
@@ -0,0 +1,14 @@
+import Bottom from "./Side";
+import Home from "./op";
+
+
+function App() {
+  return (
+    <div className="App">
+      
+       <Home/>
+    </div>
+  );
+}
+
+export default App;
